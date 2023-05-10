f4b7dd051c7d8843f388f2b1e10794b08f56764b
rahul <rahul@vocso.com>
2023-05-10 17:52:05 +0530

    a

--- backend/src/socket.io/messageDirectPrivate.js
+++ backend/src/socket.io/messageDirectPrivate.js
@@ -3,12 +3,12 @@ import Utils from '../utils';
 
 const messageDirectPrivate = async (messageData) => {
   Utils.logger.info(JSON.stringify(messageData));
-  Utils.io.to(messageData.to).emit('messageDirectPrivate', {
-    // uuid: data.uuid,
-    content: xss(messageData),
-    // name: xss(data.name),
-    // email: xss(data.email),
-  });
+  // Utils.io.to(messageData.to).emit('messageDirectPrivate', {
+  //   // uuid: data.uuid,
+  //   content: xss(messageData),
+  //   // name: xss(data.name),
+  //   // email: xss(data.email),
+  // });
   console.log(`Message sent to device with socket ID ${messageData.to}: ${messageData}`);
 };
 
