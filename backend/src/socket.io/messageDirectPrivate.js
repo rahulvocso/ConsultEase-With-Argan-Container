6dc46cdd9e5a1574951600163b79beacb97d0490
rahul <rahul@vocso.com>
2023-05-10 17:51:34 +0530

    as

--- backend/src/socket.io/messageDirectPrivate.js
+++ backend/src/socket.io/messageDirectPrivate.js
@@ -1,7 +1,7 @@
 import xss from 'xss';
 import Utils from '../utils';
 
-const messageDirectPrivate = async (messageData, callback) => {
+const messageDirectPrivate = async (messageData) => {
   Utils.logger.info(JSON.stringify(messageData));
   Utils.io.to(messageData.to).emit('messageDirectPrivate', {
     // uuid: data.uuid,
